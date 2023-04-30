96a1085b22d6e893f888088079ec3b978d539ddd
NicolasEspinoza20 <ni.espinozat@duocuc.cl>
2023-04-30 19:56:46 -0400

    commit 2.0

--- colegio/core/urls.py
+++ colegio/core/urls.py
@@ -4,9 +4,5 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('',views.anotaciones,name='anotaciones'),
-    path('asistencia',views.asistencia,name='asistencia'),
-    path('login',views.login,name='login'),
-    path('menu',views.menu,name='menu'),
-    path('notas',views.notas,name='notas'),
+    path('',views.login,name='login'),
 ]
