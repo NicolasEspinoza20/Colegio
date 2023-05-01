65611a38ddf82cab70288d821f6d512de5330def
NicolasEspinoza20 <ni.espinozat@duocuc.cl>
2023-04-30 20:20:26 -0400

    comiisss

--- colegio/core/views.py
+++ colegio/core/views.py
@@ -1,17 +1,3 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def anotaciones(request):
-    return render(request,'core/anotaciones.html')
-
-def asistencia(request):
-    return render(request,'core/asistencia.html')
-
-def login(request):
-    return render(request,'core/login.html')
-    
-def meni(request):
-    return render(request,'core/menu.html')
-
-def notas(request):
-    return render(request,'core/notas.html')
